d4869b744ffe2ed9d62a7b8952002ced314fc669
Jkean2012 <Jkean2012@outlook.com>
2025-03-04 11:02:09 -0500

    Create uv.config.js

---
+++ js/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/js/uv/science/',
+    bare:'https://fevrwbtehrtbyehrehtrjryjyrhfvdzsswdthdzazevfvbghhjk.vercel.app/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/js/uv/uv.handler.js',
+    bundle: '/js/uv/uv.bundle.js',
+    config: '/js/uv/uv.config.js',
+    sw: '/js/uv/uv.sw.js',
+};
